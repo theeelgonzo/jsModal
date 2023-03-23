99dd5eef588586883183def617d02e3e0517cc92
Jordan Godwin <jordan.godwin13@gmail.com>
2023-03-22 19:59:18 -0500

    Add closeModal function for DRY code

--- script.js
+++ script.js
@@ -14,7 +14,11 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
   });
 }
 
-btnCloseModal.addEventListener('click', function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
