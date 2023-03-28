c8aa02225b149416e48e5c261c2760fbf47c3bbd
Jordan Godwin <jordan.godwin13@gmail.com>
2023-03-27 19:43:57 -0500

    Add esc key closeModal functionality.

--- script.js
+++ script.js
@@ -22,3 +22,12 @@ const closeModal = function () {
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+  console.log(`The key ${e.key} was pressed!`);
+
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
